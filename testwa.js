88c6b7625132cd1f3d3dbb0bebcad45b1573ec0f
Rizki Hilmi <48423060+rizkihilmi@users.noreply.github.com>
2025-12-05 01:21:01 +0700

    Update testwa.js

--- testwa.js
+++ testwa.js
@@ -97,7 +97,7 @@ function kataakhir(){
       },}).go();
 }
 function muncultombol2(){fungtom2=1;TombolWA.style="opacity:1;transform:scale(1)";}
-function menuju(){if(fungtom2==1){    window.location = "https://api.whatsapp.com/send?phone=6281234567890&text="
+function menuju(){if(fungtom2==1){    window.location = "https://api.whatsapp.com/send?phone=6285775350339&text="
       + encodeURIComponent(pesanwhatsapp);}}
 
 tinggi = iniakhir.offsetHeight;
